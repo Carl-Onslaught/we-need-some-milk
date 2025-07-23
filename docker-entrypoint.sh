680368ed53192cff40d5747452fba802b26afcc7
Kuiamenh <quiamenhsarza@gmail.com>
2025-07-23 21:11:14 +0800

    Refactor MongoDB environment variables and user creation script for clarity

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
 set -e
 
-mongosh <<EOF
-var db = connect("mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@localhost:27017/admin")
-db = db.getSiblingDB('$MONGODB_NAME')
+echo "Creating application user '$MONGODB_USER' for database '$MONGODB_NAME'..."
 
+mongosh "mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@localhost:27017/admin" <<EOF
+use $MONGODB_NAME
 db.createUser({
-  user: '$MONGODB_USER',
-  pwd: '$MONGODB_PASSWORD',
-  roles: [{ role: 'dbOwner', db: '$MONGODB_NAME' }],
+  user: "$MONGODB_USER",
+  pwd: "$MONGODB_PASSWORD",
+  roles: [{ role: "dbOwner", db: "$MONGODB_NAME" }]
 });
 EOF
-# ...rest of your entrypoint script, e.g.:
-exec "$@"
